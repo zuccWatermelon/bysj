c556a4b40620902a597712b40ab81808e468031a
DESKTOP-6EL3TQ3\Mia <469498630@qq.com>
2018-01-15 23:14:14 +0800

    今天想把demo改成加入购物车productsFoot，但是因为导包的原因还没有调出来。虽然改了但是界面还是没变的原因是没保存。

--- Todos_Vuejs-withVuex/src/todoList.vue
+++ Todos_Vuejs-withVuex/src/todoList.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <h2>Simply todolist - vuejs</h2>
+    <!-- <h2>Simply todolist - vuejs</h2>
     <input id="add-input" v-model="todoText" @keyup.enter="addTodo" placeholder="do what?"/>
     <ul>
       <todo v-for="(todoItem, index) in todoList" :todoItem="todoItem" :index="index"></todo>
-    </ul>
+    </ul> -->
+    <prod-list :catalogId = '301' :banner = 'banner'></prod-list>
   </div>
 </template>
 
 <script>
-import Todo from './components/todo'
+// import Todo from './components/todo'
+import prodList from './components/products/goodsList/productsFoot'
 export default {
   name: 'todoList',
   components: {
-    Todo
+    prodList
   },
   data () {
     return {
-      todoText: ''
+      todoText: '',
+      banner:{"catalogId":161,"catalogType":"A01","catalogName":"云产品","catalogDesc":"中国电信依托发达的基础网络，通过“2+31+X”和数据中心互联网(DCI)等资源布局，实现云网融合和统一调度，已形成平台+应用的云计算和大数据产品体系，为全国用户提供天翼云产品服务。","remark":"云产品","statusCd":null,"createDate":"2017-11-11 10:04:25","createStaff":1,"parCatalogId":0,"updateDate":"2017-11-11 10:04:25","updateStaff":1}
     }
   },
   computed: {
