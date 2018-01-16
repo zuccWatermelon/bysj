55c6b67a27b822927c68d413cd8c38232adee728
DESKTOP-6EL3TQ3\Mia <469498630@qq.com>
2018-01-16 23:18:09 +0800

    今天把之前找的demo改了一个界面，导了包，可以运行了。但是框架和项目开发的框架不合，该有的包没有，不需要的到是挺多，不适合我这个初学者。于是去找了一个纯的官方推荐的vue框架cli，安装了各种包，运行出来了。明天尝试加入购物车界面。

--- Todos_Vuejs-withVuex/src/todoList.vue
+++ Todos_Vuejs-withVuex/src/todoList.vue
@@ -1,26 +1,23 @@
 <template>
   <div id="app">
-    <!-- <h2>Simply todolist - vuejs</h2>
+    <h2>Simply todolist - vuejs</h2>
     <input id="add-input" v-model="todoText" @keyup.enter="addTodo" placeholder="do what?"/>
     <ul>
       <todo v-for="(todoItem, index) in todoList" :todoItem="todoItem" :index="index"></todo>
-    </ul> -->
-    <prod-list :catalogId = '301' :banner = 'banner'></prod-list>
+    </ul>
   </div>
 </template>
 
 <script>
-// import Todo from './components/todo'
-import prodList from './components/products/goodsList/productsFoot'
+import Todo from './components/todo'
 export default {
   name: 'todoList',
   components: {
-    prodList
+    Todo
   },
   data () {
     return {
-      todoText: '',
-      banner:{"catalogId":161,"catalogType":"A01","catalogName":"云产品","catalogDesc":"中国电信依托发达的基础网络，通过“2+31+X”和数据中心互联网(DCI)等资源布局，实现云网融合和统一调度，已形成平台+应用的云计算和大数据产品体系，为全国用户提供天翼云产品服务。","remark":"云产品","statusCd":null,"createDate":"2017-11-11 10:04:25","createStaff":1,"parCatalogId":0,"updateDate":"2017-11-11 10:04:25","updateStaff":1}
+      todoText: ''
     }
   },
   computed: {
