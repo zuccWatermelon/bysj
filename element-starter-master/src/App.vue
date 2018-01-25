f6cc4f96ba1874d44a9c502663df230a36cbaad2
DESKTOP-6EL3TQ3\Mia <469498630@qq.com>
2018-01-25 18:25:27 +0800

    今天用了饿了么组件跑了一个导航栏还有弹框，学会了用组件还有methods，methods是加个逗号然后继续写下去。template里面可以加div，在最下面写style，大部分和html差不多。

--- element-starter-master/src/App.vue
+++ element-starter-master/src/App.vue
@@ -1,24 +1,53 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <el-button @click="startHacking">Start</el-button>
+    <div class="top">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="2"> 首 页 </el-menu-item>
+        <el-menu-item index="4"><a href="https://www.ele.me" target="_blank"> 合作伙伴 </a></el-menu-item>
+        <el-submenu index="3">
+        <template slot="title"> 管理平台 </template>
+            <el-menu-item index="3-1">个人管理平台</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="5"> 帮助中心 </el-menu-item>
+        <el-menu-item index="6"> 注 册 </el-menu-item>
+        <el-menu-item index="7"><el-button type="text" @click="open">登录</el-button> </el-menu-item>
+      </el-menu>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    startHacking () {
-      this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
+    data() {
+      return {
+        activeIndex: '2'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        return 0;
+      },
+      open() {
+        this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+            callback: action => {
+              this.$message({
+                type: 'info',
+                message: `action: ${ action }`
+              });
+            }
+          });
+        }
+
     }
-  }
+
 }
 </script>
 
@@ -26,5 +55,8 @@ export default {
 #app {
   font-family: Helvetica, sans-serif;
   text-align: center;
+}
+.top{
+  margin-left: 50px;
 }
 </style>
