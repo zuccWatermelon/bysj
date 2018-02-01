d4833d68dfe227dfda49feab2da0046139ea7708
DESKTOP-6EL3TQ3\Mia <469498630@qq.com>
2018-02-01 17:26:39 +0800

    今天写了购物车下拉框和底部提交栏，接下来想先写个后端，把新装跑出来，再写其他退订 续订 功能。 注意：Component template should contain exactly one root element。template中根目录下只能包含一个,应该全部写入根目录(<div>)里

--- my-project/src/components/ShoppingCar.vue
+++ my-project/src/components/ShoppingCar.vue
@@ -41,53 +41,94 @@
     <h4 align="left">杭州XXX有限公司的购物车</h4>
     <hr />
     </div>
-    <div class="table">
+<!-- 当el-table元素中注入data对象数组后，在el-table-column中用prop属性来对应对象中的键名即可填入数据，用label属性来定义表格的列名。可以使用width属性来定义列宽。 -->
+
+<div class="selectedTable">
     <el-table
-    :data="tableData4"
+    ref="multipleTable"
+    :data="tableData3"
+    tooltip-effect="dark"
     style="width: 100%"
-    max-height="250">
+    @selection-change="handleSelectionChange">
+    <el-table-column type="expand">
+ <template slot-scope="props">
+        <el-form label-position="left" inline class="demo-table-expand">
+          <el-form-item label="商品名称">
+            <span>{{ props.row.name }}</span>
+          </el-form-item>
+          <el-form-item label="单价">
+            <span>{{ props.row.price }}</span>
+          </el-form-item>
+          <el-form-item label="折扣">
+            <span>{{ props.row.discount }}</span>
+          </el-form-item>
+          <el-form-item label="总价">
+            <span>{{ props.row.amount }}</span>
+          </el-form-item>
+          <el-form-item label="状态">
+            <span>{{ props.row.state }}</span>
+          </el-form-item>
+          <el-form-item label="CPU数">
+            <span>{{ props.row.cupnum }}</span>
+          </el-form-item>
+          <el-form-item label="宽带">
+            <span>{{ props.row.net }}</span>
+          </el-form-item>
+        </el-form>
+      </template>
+      </el-table-column>
     <el-table-column
-      fixed
-      prop="name"
-      label="商品名称"
-      width="150">
+      type="selection"
+      width="55">
     </el-table-column>
     <el-table-column
-      prop="discount"
-      label="商品折扣"
-      width="120">
-    </el-table-column>
+        prop="name"
+        label="商品名称"
+        width="180">
+      </el-table-column>
     <el-table-column
-      prop="price"
-      label="单价"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="amount"
-      label="总计"
-      width="300">
-    </el-table-column>
-    <el-table-column
-      prop="state"
-      label="状态"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      fixed="do"
-      label="操作"
-      width="120">
+        prop="price"
+        label="单价"
+        width="180">
+      </el-table-column>
+       <el-table-column
+        prop="discount"
+        label="折扣"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="amount"
+        label="总价"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="state"
+        label="状态"
+        width="180">
+      </el-table-column>
+    <el-table-column label="操作">
       <template slot-scope="scope">
         <el-button
-          @click.native.prevent="deleteRow(scope.$index, tableData4)"
-          type="text"
-          size="small">
-          移除
-        </el-button>
+          size="mini"
+          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+        <el-button
+          size="mini"
+          type="danger"
+          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
       </template>
     </el-table-column>
   </el-table>
 </div>
+      <div class="bottom">
+      <div class="bottom-center">
+        <div class="submit" @click="submitOrder()">提交</div>
+        <div class="final-price">
+          <p class="word">总价：<span class="orange sumPrice"><small>￥80</small></span></p>
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -101,21 +142,26 @@ export default {
           pwd: ''
         },
         formLabelWidth: '40px',
-        tableData4: [{
-          name:'云产品',
-          discount:'0.8',
-          price:'100',
-          amount:'80',
-          state:'待提交',
-          do:'删除',
-        }, {
-          name:'云产品',
-          discount:'0.8',
-          price:'100',
-          amount:'80',
-          state:'待提交',
-          do:'删除',
-        }]
+
+        tableData3: [{
+            name: '云主机',
+            price:'100',
+            discount:'0.8',
+            amount:'80',
+            state:'待提交',
+            cupnum:'2',
+            net:'2m'
+           
+          },{
+            name: '云主机',
+            price:'100',
+            discount:'0.8',
+            amount:'80',
+            state:'待提交',
+            cupnum:'2',
+            net:'2m'
+          }],
+        multipleSelection: [],     
       };
     },
     methods: {
@@ -128,10 +174,138 @@ export default {
           cancelButtonText: '取消',
         });
       },
-      deleteRow(index, rows) {
-        rows.splice(index, 1);
+        toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      handleEdit(index, row) {
+        console.log(index, row);
+      },
+      handleDelete(index, row) {
+        console.log(index, row);
+      },
+      submitOrder:function (){
+
       }
-      
+      //       /**提交订单**/
+      // submitOrder:function () {
+      //   var _this = this;
+      //   var param = {};
+      //   var orderItemList = [];
+      //   var num = 0;
+
+      //   /**遍历所有的购物车行,一次只允许提交一个**/
+      //   $(".item-li").each(function () {
+      //     var node = $(this).find("input[type='checkbox']");
+      //     if($(node).prop("checked")) {
+      //       num++;
+      //     }
+      //   });
+
+      //   if(num != 1){
+      //     this.$alert('不支持一次性提交多项!','提示',{
+      //       confirmButtonText: '确定',
+      //       callback: action => {
+      //         return false;
+      //       }
+      //     })
+      //   }
+
+      //   if(num == 0){
+      //     this.$alert('请先选中再提交!','提示',{
+      //       confirmButtonText: '确定',
+      //       callback: action => {
+      //         return false;
+      //       }
+      //     })
+      //   }
+
+      //   /**提交成功后返回信息**/
+      //   var successMsg = "";
+      //   /**提交失败信息**/
+      //   var errorMsg = "";
+      //   /**组装提交的数据**/
+      //   $("li.item-li").each(function () {
+      //     var node = this;
+      //     /**如果该checkbox被选中**/
+      //     if($(node).find(".main-top").find("input[type='checkbox']").prop("checked")) {
+      //       var cartId = $(node).find(".inputCartId").val();
+      //       var orderItemId = $(node).find(".inputCartItemId").val();
+      //       var applyObjId = $(node).find(".inputOfferId").val();
+      //       var catalogId = $(node).find(".inputCatalogId").val();
+      //       var applyObjSpec = "1";
+      //       var discount = $(node).find(".discount").val();
+      //       var billType = $(node).find(".billType").val();
+      //       var offerId = $(node).find(".inputOfferId").val();
+      //       var itemCdName = $(node).find(".inputItemCdName").val();
+      //       var itemCd = $(node).find(".inputItemCd").val();
+      //       var statusCd = $(node).find(".inputStatusCd").val();
+
+      //       var amount = $.trim($(node).find(".totalPrice").text());
+
+      //       /**IPRAN产品需要增加一次性费用原价以及一次性费用折扣**/
+      //       var discountOne = "";   //一次性费用折扣
+      //       var amountOne = "";     //一次性费用金额
+      //       if(offerId == '261' || offerId == '321' || offerId == '322' || offerId == '323'){
+      //         discountOne = $(node).find(".onceChargeDiscount").val();
+      //         let txt = $(node).find(".onceChargePriceAfterDiscount").text();
+      //         amountOne = txt.substring(1,txt.length);
+      //       }
+
+      //         successMsg = "订单已成功提交!"
+          
+      //       param.inputItemCdName = itemCdName;
+      //       param.code = offerId;
+      //       param.cartId = cartId;
+      //       param.itemId = orderItemId;
+      //       param.applyObjId = applyObjId;
+      //       param.applyObjSpec = applyObjSpec;
+      //       param.discount = discount;
+      //       param.billType = billType;
+      //       param.catalogId = catalogId;
+      //       param.statusCd = statusCd;
+      //       param.itemCd = itemCd;
+      //       param.amount = amount;
+      //       param.offerId = offerId;
+      //       param.discountOne = discountOne;
+      //       param.amountOne = amountOne;
+      //       if(_this.parentOrderTransInstance.duration != null && _this.parentOrderTransInstance.duration != ''){
+      //         param.duration = _this.parentOrderTransInstance.duration;
+      //       }
+
+      //       if(_this.parentOrderTransInstance.payType != null && _this.parentOrderTransInstance.payType != ''){
+      //         param.payType = _this.parentOrderTransInstance.payType;
+      //       }
+      //     }
+      //   });
+      //   /**报错信息**/
+      //   var msg = "";
+      //   /**判断购物车能否提交**/
+      //     axios({
+      //       method:'post',
+      //       url:'/netCloudOrder/shoppingCartService/checkShoppingCart',
+      //       data:{
+      //         cartItemId:param.itemId,
+      //         catalogId:param.catalogId,
+      //       },
+      //     }).then(res=>{
+      //       /**如果可以提交**/
+      //         msg = res.data.msg;
+      //         _this.$alert(msg,'提示',{
+      //           confirmButtonText: '确定',
+      //           callback: action => {
+      //           }
+      //         })
+      //     })
+      // },
     }
 }
 </script>
@@ -153,7 +327,66 @@ export default {
   margin-left: 100px;
   margin-right: 100px; 
 }
-.table{
+.selectedTable{
   margin-left: 100px;
+  margin-right: 100px;
+  text-align: left;
 }
+
+  .demo-table-expand {
+    font-size: 0;
+  }
+  .demo-table-expand label {
+    width: 90px;
+    color: #99a9bf;
+  }
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 50%;
+  }
+   .bottom{
+    width:100%;
+    margin-top:50px;
+    position: fixed;
+    bottom: 0px;
+    background-color: #e5e5e5;
+    z-index: 10000;
+  }
+
+  .bottom-center{
+    width:1200px;
+    margin:0 auto;
+}
+.final-price{
+      width:650px;
+      height:50px;
+      margin-top: 20px;
+      margin-right:10px;
+      float:right;
+      text-align: right;
+    }
+.word{
+        line-height: 35px;
+}
+.submit{
+      width:130px;
+      height:70px;
+      float: right;
+      text-align: center;
+      line-height: 70px;
+      background-color: #ff8200;
+      font-size: 25px;
+      color:#fff;
+      cursor: pointer;
+    }
+    .orange{
+        font-weight: bold;
+        font-size: 26px;
+        color:#ff8200;
+      }
+      .light-word{
+        font-size: 12px;
+        color:#999;
+      }
 </style>
