be981cc9db2aa5371727aadc96399339a0221409
DESKTOP-6EL3TQ3\Mia <469498630@qq.com>
2018-03-10 20:04:24 +0800

    继续研究更新续订规则。。。

---
+++ Keystone/routes/api/orderChange.js
@@ -0,0 +1,50 @@
+var keystone = require('keystone');
+
+var order = keystone.list('OrderItem');
+var cpu =  keystone.list('Cpu');
+var memory = keystone.list('Memory');
+var operatesystemtype = keystone.list('OperateSystemType');
+var operatesystem = keystone.list('OperateSystem');
+var hardDiskType = keystone.list('HardDiskType');
+var customer = keystone.list('Customer');
+var async = require('async');//异步的
+
+exports = module.exports = function (req, res) {
+    var cpu = req.body.cpu;
+    var memory = req.body.memory;
+    var operateSystemType = req.body.operateSystemType;
+    var operateSystem = req.body.operateSystem;
+    var systemHardDiskType = req.body.systemHardDiskType;
+    var dataHardDiskType = req.body.dataHardDiskType;
+    var dataHardDiskSize = req.body.dataHardDiskSize;
+    var bandWidth = req.body.bandWidth;
+    var status = req.body.status;
+    var period = req.body.period;
+    var returnMessag = {}; 
+    var userID = req.body.userID;
+
+    console.log(userID)
+
+    var newOrder = new order.model({
+            Cpu: '5a7970d6bc9a5b0370dae3a8',
+            memory: '5a7970f5bc9a5b0370dae3ae',
+            operateSystem:'5a79718dbc9a5b0370dae3b9',
+            operateSystemType:'5a79717cbc9a5b0370dae3b7',
+            systemHardDiskType:'5a7971b5bc9a5b0370dae3bc',
+            dataHardDiskType:'5a7971b5bc9a5b0370dae3bc',
+            dataHardDiskSize:'40',
+            period:'1',
+            bandWidth:'1',
+            status:'待提交',
+            userID:userID
+    })
+// if (shouldBePublished) {
+//     newOrder.state = 'published';
+// }
+    newOrder.save(function(err) {
+        console.log(err)
+        // post已保存  
+    });
+    returnMessag.message = "添加订单成功";
+    res.send(JSON.stringify(returnMessag));
+};
